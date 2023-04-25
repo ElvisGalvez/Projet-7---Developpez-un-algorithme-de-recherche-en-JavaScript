7ba645ff499ce8e1d84f5d8074c65d6be27fcb1d
ElvisGalvez <galvez.elvis@gmail.com>
2023-04-25 18:33:20 +0200

    front

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+import { recipes } from '/data/recipes.js';
+
+const recipesSection = document.querySelector('.recipes');
+const recipeCardTemplate = document.getElementById('recipe-card-template');
+
+recipes.forEach(recipe => {
+  const recipeCard = recipeCardTemplate.content.cloneNode(true);
+
+  recipeCard.querySelector('.recipe-card__title').textContent = recipe.name;
+  
+  const timeElement = recipeCard.querySelector('.recipe-card__time');
+  const timeText = document.createTextNode(`${recipe.time} min`);
+  const timeIcon = document.createElement('i');
+  timeIcon.classList.add('far', 'fa-clock', 'recipe-card__time__icon');
+  timeElement.appendChild(timeIcon);
+  timeElement.appendChild(timeText);
+
+  recipeCard.querySelector('.recipe-card__description').textContent = recipe.description;
+
+  const ingredientsList = recipeCard.querySelector('.recipe-card__ingredients');
+  recipe.ingredients.forEach(ingredient => {
+    const ingredientItem = document.createElement('li');
+    ingredientItem.textContent = `${ingredient.ingredient} - ${ingredient.quantity} ${ingredient.unit || ''}`;
+    ingredientsList.appendChild(ingredientItem);
+  });
+
+  recipesSection.appendChild(recipeCard);
+});
